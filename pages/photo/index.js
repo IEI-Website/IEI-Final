8484cd2d6f293c59b7da24006ad1f922379116b6
kaliappan <skaliappan1908@gmail.com>
2022-04-26 11:28:54 +0530

    Feature : photo gallery

--- pages/photo/index.js
+++ pages/photo/index.js
@@ -1,34 +1,57 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import {Center} from "@chakra-ui/react";
-import VideoBackground from '../components/VideoBackground';
+import {Center, Heading, SimpleGrid} from "@chakra-ui/react";
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import NavBa from '../components/Navba';
+import Card from './Card';
+
 export default function Home() {
+  
   return (
     <div>
       <Head>
-        <title>Photo Gallery</title>
+        <title>Registration- IEI-BLC FCRIT</title>
         <meta name="title" content="IEI-BLC FCRIT" />
 
       </Head>
-      <VideoBackground />
-      <Header/><NavBa />
+      <Header/>
       <Center  fontSize="6xl" fontWeight="bold" color="white">
-        Photo Gallery
+        Registration
       </Center>
       <br/>
       <Center  fontSize="3xl" color="white">
-        <Image src="/fcritlogo.png"width={200} height={200} />
-        <Image src="/iei.png"width={200} height={200} />
+        <Image src="/iei.png"width={300} height={300} />
+        <Image src="/fcritlogo.png"width={300} height={300} />
       </Center>
       <br/>
       <Center fontSize="4xl" color="white">
         Awards for academic excellence
       </Center>
+      <Center>
+        <Heading>
+          Photo Gallery
+        </Heading>
+      </Center>
       <br/><br/><br/>
-      <Footer/>
+      <SimpleGrid columns={3}  spacingX='40px' spacingY='20px'>
+      <Card personName={"K K Varkhedkar"} image="/photos/KK-varkhedkar.jpeg" designation="Lorem" /> 
+      <Card personName={"Mr Kamble"} image="/photos/kamble.jpeg" designation="Lorem" /> 
+      <Card personName={"Godbole"} image="/photos/godbole.jpeg" designation="Lorem" /> 
+      <Card personName={"Dr Nimkar"} image="/photos/nimkar.jpeg" designation="Lorem" /> 
+      <Card personName={"Mr Sanjay"} image="/photos/sanjay.jpeg" designation="Lorem" /> 
+      <Card personName={"Fr saturnino Almeida"} image="/photos/Fr.jpeg" designation="Lorem" /> 
+      <Card personName={"Dr S M  Khot"} image="/photos/sm-khot.jpeg" designation="Lorem" /> 
+      <Card personName={"Dr Nilaj Deshmukh"} image="/photos/dr-nilaj-deshmukh.jpeg" designation="Lorem" /> 
+      <Card personName={"Dr Bindu S"} image="/photos/Bindu.jpeg" designation="Lorem" /> 
+      <Card personName={"Dr Milind Shah"} image="/photos/milind-shah.jpeg" designation="Lorem" /> 
+      <Card personName={"Dr Lata Ragha"} image="/photos/lata.jpeg" designation="Lorem" /> 
+      <Card personName={"Dr Sushil Thale"} image="/photos/sushil.jpeg" designation="Lorem" /> 
+      </SimpleGrid>
+
+
+
+    <Footer/>
     </div>
+
   )
 }

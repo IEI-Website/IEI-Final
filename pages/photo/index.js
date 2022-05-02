0f3f58c0dc78b5c67c604e9360c87cb5fc66a793
gregbg218 <hvgreg01@gmail.com>
2022-05-02 20:05:20 +0530

    gallery cleared

--- pages/photo/index.js
+++ pages/photo/index.js
@@ -72,42 +72,11 @@ export default function Home() {
           <p className="legend">Image 8</p>
         </div>
       </Carousel>
+<br/>
+<br/>
+<br/>
 
-
-      <HStack>
-        <Box backgroundColor="black" h="3px" marginLeft="5%" w="30%" />
-        <Heading >
-          IEI Committee Members
-        </Heading>
-        <Box backgroundColor="black" h="3px" marginRight="5%" w="30%" />
-      </HStack>
-      <br /><br />
-      <SimpleGrid paddingBottom="20" columns={3} spacingX='20px' spacingY='20px'>
-        <Card personName={"Mr. K K Varkhedkar"} image="/photos/KK-varkhedkar.jpeg" />
-        <Card personName={"Mr. Kamble"} image="/photos/kamble.jpeg" />
-        <Card personName={"Mr. Mohan"} image="/photos/mohan.jpg" />
-        <Card personName={"Mr. Bhandarkar"} image="/photos/mr_bhandarkar.jpeg" />
-        <Card personName={"Mr. Godbole"} image="/photos/godbole.jpeg" />
-        <Card personName={"Dr. Nimkar"} image="/photos/nimkar.jpeg" />
-        <Card personName={"Mr. Sanjay"} image="/photos/sanjay.jpeg" />
-      </SimpleGrid>
-      <HStack>
-        <Box backgroundColor="black" h="2px" marginLeft="5%" w="40%" />
-        <Heading>
-          FCRIT
-        </Heading>
-        <Box backgroundColor="black" h="2px" marginRight="5%" w="40%" />
-      </HStack>
-      <br /><br />
-      <SimpleGrid paddingBottom="100" columns={3} spacingX='40px' spacingY='20px'>
-        <Card personName={"Fr. Saturnino Almeida"} image="/photos/Fr.jpeg" />
-        <Card personName={"Dr. S M  Khot"} image="/photos/sm-khot.jpeg" />
-        <Card personName={"Dr. Nilaj Deshmukh"} image="/photos/dr-nilaj-deshmukh.jpeg" />
-        <Card personName={"Dr. Bindu S"} image="/photos/Bindu.jpeg" />
-        <Card personName={"Dr. Milind Shah"} image="/photos/milind-shah.jpeg" />
-        <Card personName={"Dr. Lata Ragha"} image="/photos/lata.jpeg" />
-        <Card personName={"Dr. Sushil Thale"} image="/photos/sushil.jpeg" />
-      </SimpleGrid>
+      
 
       <Footer />
     </div>

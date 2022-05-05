adb71685eadda24ac9db28a23834504f9c665846
gregbg218 <hvgreg01@gmail.com>
2022-05-05 11:49:50 +0530

    added dropdown with tables

--- pages/components/Navba.js
+++ pages/components/Navba.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, Box, Flex, Text, Button, Stack, Menu, MenuButton, MenuList, MenuItemOption, MenuGroup, MenuOptionGroup, MenuDivider, } from "@chakra-ui/react";
 import { ChevronDownIcon, ChevronRightIcon } from '@chakra-ui/icons'
-import DropdownCategories from "./DropdownCategories";
+
 const NavBa = (props) => {
   const [isOpen, setIsOpen] = React.useState(false);
 
@@ -73,15 +73,15 @@ const MenuLinks = ({ isOpen }) => {
           Home
         </MenuItem>
 
-        
+
 
         <Menu>
           <MenuButton as={Button} bgColor="gray" color={"black"} rightIcon={<ChevronDownIcon />}>
             Committees
           </MenuButton>
-          <MenuList  minWidth='240px' color={"black"} bgColor="gray.100">
-            <MenuItem align ="center"to="/committees/adv" color={"black"}>Advisory Committee  </MenuItem>
-            <MenuItem align ="center"to="/committees"  color={"black"}>Organising Committee </MenuItem>
+          <MenuList minWidth='240px' color={"black"} bgColor="gray.100">
+            <MenuItem align="center" to="/committees/adv" color={"black"}>Advisory Committee  </MenuItem>
+            <MenuItem align="center" to="/committees" color={"black"}>Organising Committee </MenuItem>
           </MenuList></Menu>
 
         {/* <Menu>
@@ -96,50 +96,41 @@ const MenuLinks = ({ isOpen }) => {
           </MenuList>
         </Menu> */}
         <Menu>
-          <MenuButton as={Button} bgColor="gray" color={"black"} rightIcon={<ChevronDownIcon />}>
+          <MenuButton as={Button} isOpen={'true'} bgColor="gray" color={"black"} rightIcon={<ChevronDownIcon />}>
             Categories
           </MenuButton>
           <MenuList minWidth='240px' color={"black"} bgColor="gray.100">
-          <MenuOptionGroup defaultValue='asc' title='Research Excellence' type='radio'>
-            <MenuOptionGroup defaultValue='asc' title='Regional' type='radio'>
-            <MenuItem  align="center" to='/categories/researchExcellenceStudent' value='desc'>Students</MenuItem>
-            <MenuItem align="center" to='/categories/researchExcellenceTeacher' value='asc'>Teaching Faculty</MenuItem>
+
+            <MenuOptionGroup fontSize={'17px'} align="center" title='Research Excellence' type='checkbox'>
+              <MenuOptionGroup defaultValue='asc' type='radio'>
+                <MenuItem align="center" to='/categories/regionalResearch' value='desc'>Regional</MenuItem>
+                <MenuItem align="center" to='/categories/nationalResearch' value='asc'>National</MenuItem>
+              </MenuOptionGroup>
             </MenuOptionGroup>
-            <MenuOptionGroup defaultValue='asc' title='National' type='radio'>
-            <MenuItem align="center" to='/categories/researchExcellenceStudent' value='desc'>Students</MenuItem>
-            <MenuItem align="center" to='/categories/researchExcellenceTeacher' value='asc'>Teaching Faculty</MenuItem>
-            <MenuItem align="center" to='/categories/researchExcellenceIndustry' value='desc'>Researchers</MenuItem>
+
+            <MenuDivider />
+
+            <MenuOptionGroup fontSize={'17px'} align="center" title='Academic Excellence' type='checkbox'>
+              <MenuOptionGroup defaultValue='asc' type='radio'>
+                <MenuItem align="center" to='/categories/regionalAcademic' value='desc'>Regional</MenuItem>
+                <MenuItem align="center" to='/categories/nationalAcademic' value='asc'>National</MenuItem>
+              </MenuOptionGroup>
             </MenuOptionGroup>
-            
-          </MenuOptionGroup>
-          <MenuDivider />
-          <MenuOptionGroup title='Academic Excellence' type='checkbox'>
-          <MenuOptionGroup defaultValue='asc' title='Regional' type='radio'>
-            <MenuItem  align="center" to='/categories/academicExcellenceStudent' value='desc'>Students</MenuItem>
-            <MenuItem align="center" to='/categories/academicExcellenceTeacher' value='asc'>Teaching Faculty</MenuItem>
-            <MenuItem align="center" to='/categories/academicExcellenceHOD' value='desc'>Head of the Department</MenuItem>
-            <MenuItem align="center" to='/categoriesacademicExcellencePrincipal' value='asc'>Principal/Director</MenuItem>
-            <MenuItem align="center" to='/categories/academicExcellenceInstitute' value='desc'>Institute</MenuItem>
-            </MenuOptionGroup>
-            <MenuOptionGroup defaultValue='asc' title='National' type='radio'>
-            <MenuItem  align="center" to='/categories/academicExcellenceStudent' value='desc'>Students</MenuItem>
-            <MenuItem align="center" to='/categories/academicExcellenceTeacher' value='asc'>Teaching Faculty</MenuItem>
-            <MenuItem align="center" to='/categories/academicExcellenceHOD' value='desc'>Head of the Department</MenuItem>
-            <MenuItem align="center" to='/categories/academicExcellencePrincipal' value='asc'>Principal/Director</MenuItem>
-            <MenuItem align="center" to='/categories/academicExcellenceInstitute' value='desc'>Institute</MenuItem>
-            </MenuOptionGroup>
-          </MenuOptionGroup>
-          <MenuDivider />
-          <MenuOptionGroup defaultValue='asc' title='Industry Excellence Award' type='radio'>
-          <MenuItem align ="center" to="/categories/industryExcellence" color={"black"}>Industry Excellence Award</MenuItem>
-          </MenuOptionGroup>
-          <MenuDivider />
-          <MenuOptionGroup defaultValue='asc' title='Industry Excellence Award' type='radio'>
-          <MenuItem align ="center"to="/categories/startupExcellence" color={"black"}>Startup Excellence Award</MenuItem>
-          </MenuOptionGroup>
+
+            <MenuDivider />
+
+
+            <MenuItem fontWeight={'bold'} align="center" to="/categories/industryExcellence" color={"black"}>Industry Excellence Award</MenuItem>
+
+
+            <MenuDivider />
+
+            <MenuItem fontWeight={'bold'} align="center" to="/categories/startupExcellence" color={"black"}>Startup Excellence Award</MenuItem>
+
+
           </MenuList>
         </Menu>
-        
+
         <MenuItem as={Button} bgColor="gray" color={"black"} to="/imp">
           Imp Dates
         </MenuItem>
@@ -149,10 +140,10 @@ const MenuLinks = ({ isOpen }) => {
             Registration
           </MenuButton>
           <MenuList minWidth='240px' color={"black"} bgColor="gray.100">
-            <MenuItem align ="center"to="/categories/generalInstructions" color={"black"}>Instructions </MenuItem>
-            <MenuItem align ="center"to="/register/regdet" color={"black"}>Registration Details </MenuItem>
-            <MenuItem align ="center"to="/register" color={"black"}>Registration Form</MenuItem>
-            <MenuItem align ="center"to="/register" color={"black"}>Payment Details</MenuItem>
+            <MenuItem align="center" to="/categories/generalInstructions" color={"black"}>Instructions </MenuItem>
+            <MenuItem align="center" to="/register/regdet" color={"black"}>Registration Details </MenuItem>
+            <MenuItem align="center" to="/register" color={"black"}>Registration Form</MenuItem>
+            <MenuItem align="center" to="/register" color={"black"}>Payment Details</MenuItem>
           </MenuList>
         </Menu>
 
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import {Center,Flex,Text,HStack,VStack,Box,Heading, SimpleGrid,} from "@chakra-ui/react";
+import { Center, Flex, Text, HStack, VStack, Box, Heading, SimpleGrid, } from "@chakra-ui/react";
 import VideoBackground from "../pages/components/VideoBackground";
 import Header from "../pages/components/Header";
 import Footer from './components/Footer';
@@ -27,50 +27,51 @@ export default function Home() {
       </Head>
       <VideoBackground />
       <div>
-        <Header/>
+        <Header />
         <NavBa />
       </div>
-     <br/>
-     <Flex>
-     
+      <br />
+      <Flex>
+
         <Flex color={"white"}> .</Flex>
-      <Flex width={"100%"} >
-      
-      <Box width={"100%"}  boxShadow='outline' p='6' rounded='md'>
-        <Text bgColor={"telegram.700"} fontWeight="bold">Latest Updates</Text>
-        <Marquee>Registration will start soon. Please stay tune. </Marquee>  
-      </Box>
-      
+        <Flex width={"100%"} >
+
+          <Box width={"100%"} boxShadow='outline' p='6' rounded='md'>
+            <Text bgColor={"telegram.700"} fontWeight="bold">Latest Updates</Text>
+            <Marquee>Registration will start soon. Please stay tune. </Marquee>
+          </Box>
+
+        </Flex>
       </Flex>
-      </Flex>
-      
-      <br/>
-      
-      
-      
-      <br/><br/><br/>
-      <Flex overflow={'hidden'}  alignContent={'center'} marginRight={"full"}>
-        <Carousel width={'100%'} >
-        <div>
-              <img  src="/monument2.jpg" alt="image1"/>
-                <p className="legend">IEI-BLC</p>
+
+      <br />
+
+
+
+      <br /><br /><br />
+      <Flex overflow={'hidden'} alignContent={'center'} marginRight={"full"}>
+        <Carousel width={'100%'} showThumbs={false}>
+          <div>
+            <img src="/monument2.jpg" alt="image1" />
+            <p className="legend">IEI-BLC</p>
           </div>
           <div>
             <img className='carousel-img' src="/2.jpg" alt="image2" />
             <p className="legend">FCRIT, Vashi</p>
           </div>
         </Carousel>
-        </Flex>
-        <br/><br/><br/>
-      <SimpleGrid bgColor={"telegram.700"} paddingTop="20"paddingBottom="20" columns={2}  spacingX='50px' spacingY='50px'>
-      <Card  aboutName={"About IEI-BLC FCRIT Awards "}  texts={"It is with great pleasure that Fr. C. Rodrigues Institute of Technology in collaboration with The Institute of Engineers, India – Belapur Local Centre is announcing the IEI BLC – FCRIT Excellence Awards – 2022. The objective of  the award ceremony is to recognize and appreciate the excellence of Academicians and Industry professionals in their respective domains. We are glad to inform that IEI BLC – FCRIT Excellence Awards organized in the years 2019 and 2021 received overwhelming responses and have been very successful."}  /> 
-      <Card  aboutName={"About The Institution of Engineers (India) "}texts={"The Institution of Engineers (India) [IEI] is the largest multidisciplinary professional body of engineers, established in 1920 with its Headquarters located in Kolkata and incorporated under Royal Charter on 9th September,1935 by the then His Majesty of King George V. The Royal Charter endowed the Institution with the responsibility to promote the general advancement of engineering amongst its members and persons attached to the Institution. The Institution is protected under Article 372 of the Constitution of India                "}  />
-      <Card  aboutName={"About IEI-Belapur Local Center"} texts={"The Council of IEI set up Belapur Local Centre (BLC) in May 1994. Ever since its inception, BLC is vibrant with active participation from its office bearers and ever-increasing members. BLC is situated in a 1000 Sq. Meters plot in Sector 15, Central Business District (CBD), close to Belapur Railway Station in Navi Mumbai. BLC Building was designed by the reputed architects, M/s.Shashi Prabhu & Associates and M/s. Sopan Parbhu Architect. At present,IEIBLC has 3687 members comprising of engineers from 15 Divisions."}   /> 
-      <Card  aboutName={"About FCRIT "} texts={"FCRIT was established in 1994, named after late Rev. Fr. Conceicao Rodrigues. FCRIT persistently seeks and adopts innovative methods to improve the quality of education on a consistent basis. The global standards set at FCRIT in the field of teaching spurs the students in relentless pursuit of excellence. Its location in the vicinity of the holy places of various religious denominations underscores its secular credentials and its philosophy of 'Vasudhaiva Kuttumbakam'. The college prides on being one of few that has accreditation for all five branches."} />
-      </SimpleGrid>
-      <br/>
 
-      <Footer/>
+      </Flex>
+      <br /><br /><br />
+      <SimpleGrid bgColor={"telegram.700"} paddingTop="20" paddingBottom="20" columns={2} spacingX='50px' spacingY='50px'>
+        <Card aboutName={"About IEI-BLC FCRIT Awards "} texts={"It is with great pleasure that Fr. C. Rodrigues Institute of Technology in collaboration with The Institute of Engineers, India – Belapur Local Centre is announcing the IEI BLC – FCRIT Excellence Awards – 2022. The objective of  the award ceremony is to recognize and appreciate the excellence of Academicians and Industry professionals in their respective domains. We are glad to inform that IEI BLC – FCRIT Excellence Awards organized in the years 2019 and 2021 received overwhelming responses and have been very successful."} />
+        <Card aboutName={"About The Institution of Engineers (India) "} texts={"The Institution of Engineers (India) [IEI] is the largest multidisciplinary professional body of engineers, established in 1920 with its Headquarters located in Kolkata and incorporated under Royal Charter on 9th September,1935 by the then His Majesty of King George V. The Royal Charter endowed the Institution with the responsibility to promote the general advancement of engineering amongst its members and persons attached to the Institution. The Institution is protected under Article 372 of the Constitution of India                "} />
+        <Card aboutName={"About IEI-Belapur Local Center"} texts={"The Council of IEI set up Belapur Local Centre (BLC) in May 1994. Ever since its inception, BLC is vibrant with active participation from its office bearers and ever-increasing members. BLC is situated in a 1000 Sq. Meters plot in Sector 15, Central Business District (CBD), close to Belapur Railway Station in Navi Mumbai. BLC Building was designed by the reputed architects, M/s.Shashi Prabhu & Associates and M/s. Sopan Parbhu Architect. At present,IEIBLC has 3687 members comprising of engineers from 15 Divisions."} />
+        <Card aboutName={"About FCRIT "} texts={"FCRIT was established in 1994, named after late Rev. Fr. Conceicao Rodrigues. FCRIT persistently seeks and adopts innovative methods to improve the quality of education on a consistent basis. The global standards set at FCRIT in the field of teaching spurs the students in relentless pursuit of excellence. Its location in the vicinity of the holy places of various religious denominations underscores its secular credentials and its philosophy of 'Vasudhaiva Kuttumbakam'. The college prides on being one of few that has accreditation for all five branches."} />
+      </SimpleGrid>
+      <br />
+
+      <Footer />
     </div>
   )
 }

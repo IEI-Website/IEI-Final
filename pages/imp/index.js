f9549d9653e2933aff41fc1b8e4c45c0fc0bda4b
gregbg218 <hvgreg01@gmail.com>
2022-09-05 19:46:22 +0530

    changed date

--- pages/imp/index.js
+++ pages/imp/index.js
@@ -22,7 +22,7 @@ export default function Home() {
       <Center fontSize={"2xl"} fontWeight="bold">Opening Date: 5th May 2022</Center><br/>
       <Center fontSize={"2xl"}fontWeight="bold">Closing Date: 30th June 2022</Center><br/>
       <Center fontSize={"2xl"}fontWeight="bold">Communication to the Awardees: 5th September 2022</Center><br/>
-      <Center fontSize={"2xl"}fontWeight="bold">Award Ceremony: 15th September 2022</Center><br/>
+      <Center fontSize={"2xl"}fontWeight="bold">Award Ceremony: 16th September 2022</Center><br/>
       <br/><br/><br/>
       <Footer/>
     </div>
